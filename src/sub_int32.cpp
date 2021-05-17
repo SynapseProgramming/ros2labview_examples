4d7ff3cd0ce3841fe0c17c879d354ad2efd37005
Roald Ong <roald.ong@gmail.com>
2021-05-17 10:10:42 +0800

    fixed issue where ros cpp programs would not receive data from labview due to a mismatch in qos settings

--- src/sub_int32.cpp
+++ src/sub_int32.cpp
@@ -1,23 +1,31 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
-
+#include <iostream>
+#define be RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT
 
 class MinimalSubscriber : public rclcpp::Node
 {
 public:
   MinimalSubscriber()
-  : Node("int_sub")
-  {
+  : Node("int_sub"),
+    best_effort(rclcpp::KeepLast(10))
+    {
     subscription_ = this->create_subscription<std_msgs::msg::Int32>(
-      "labviewint", 10, [this](const std_msgs::msg::Int32::SharedPtr msg){
+      "labviewint",best_effort.reliability(be),
+      [this](const std_msgs::msg::Int32::SharedPtr msg){
       RCLCPP_INFO(this->get_logger(), "I heard: %d", msg->data);
+      std::cout<<"callback received\n";
     });
   }
 
 private:
   rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+  rclcpp::QoS best_effort;
+
+
 };
+
 
 int main(int argc, char * argv[])
 {
