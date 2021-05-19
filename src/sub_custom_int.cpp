498a721f3fac917d041ecb6780bd7d682a0e760a
Roald Ong <roald.ong@gmail.com>
2021-05-19 11:36:15 +0800

    fixed sub_custom_int qos settings

--- src/sub_custom_int.cpp
+++ src/sub_custom_int.cpp
@@ -2,21 +2,26 @@
 #include "rclcpp/rclcpp.hpp"
 #include "ros2labview_examples/msg/myint.hpp"
 
+#define be RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT
 
 class MinimalSubscriber : public rclcpp::Node
 {
 public:
   MinimalSubscriber()
-  : Node("cint_sub")
+  : Node("cint_sub"),
+  best_effort(rclcpp::KeepLast(10))
+
   {
     subscription_ = this->create_subscription<ros2labview_examples::msg::Myint>(
-      "clabint", 10, [this](const ros2labview_examples::msg::Myint::SharedPtr msg){
+      "clabint", best_effort.reliability(be), [this](const ros2labview_examples::msg::Myint::SharedPtr msg){
       RCLCPP_INFO(this->get_logger(), "I heard: %d", msg->num);
     });
   }
 
 private:
   rclcpp::Subscription<ros2labview_examples::msg::Myint>::SharedPtr subscription_;
+
+  rclcpp::QoS best_effort;
 };
 
 int main(int argc, char * argv[])
