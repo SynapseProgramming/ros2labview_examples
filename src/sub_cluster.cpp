669c4d8e00e3358c73a9c8a61266182007ba2066
Roald Ong <roald.ong@gmail.com>
2021-05-19 10:47:35 +0800

    fixed sub_cluster qos settings

--- src/sub_cluster.cpp
+++ src/sub_cluster.cpp
@@ -2,21 +2,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "ros2labview_examples/msg/clusterdemo.hpp"
 
+#define be RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT
 
 class MinimalSubscriber : public rclcpp::Node
 {
 public:
   MinimalSubscriber()
-  : Node("cluster_sub")
+  : Node("cluster_sub"),
+    best_effort(rclcpp::KeepLast(10))
   {
     subscription_ = this->create_subscription<ros2labview_examples::msg::Clusterdemo>(
-      "intcust", 10, [this](const ros2labview_examples::msg::Clusterdemo::SharedPtr msg){
+      "intcust", best_effort.reliability(be), [this](const ros2labview_examples::msg::Clusterdemo::SharedPtr msg){
       RCLCPP_INFO(this->get_logger(), "I heard: %d and %d", msg->x,msg->y);
     });
   }
 
 private:
   rclcpp::Subscription<ros2labview_examples::msg::Clusterdemo>::SharedPtr subscription_;
+
+  rclcpp::QoS best_effort;
 };
 
 int main(int argc, char * argv[])
